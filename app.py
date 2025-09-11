2de24fddf12db26ab20c9b4526cc5d11b5941c93
Ahmed Akram Amer <a7m7da7e3@gmail.com>
2025-09-11 15:05:58 +0300

    Data Loading , Cleaning & EDA

---
+++ app.py
@@ -0,0 +1,3 @@
+from backend.eda import perform_eda
+
+perform_eda('iris.csv')
---
+++ backend/data.py
@@ -0,0 +1,98 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+
+# 1- Loading the Data
+def load_data(file_path):
+    """
+    Load data from a file into a pandas DataFrame.
+    (CSV, Excel, JSON) -> Only Tabular Data.
+    """
+
+    if file_path.endswith('.csv'):
+        df = pd.read_csv(file_path)
+    elif file_path.endswith(('.xlsx', '.xls')):
+        df = pd.read_excel(file_path)
+    elif file_path.endswith('.json'):
+        df = pd.read_json(file_path)
+    else:
+        raise ValueError(
+            'Unsupported File Format, Please use CSV, Excel or JSON Format.')
+
+    return df
+
+
+# 2- Data Cleaning
+def clean_data(df):
+    """
+    Run Full Data Cleaning Pipeline on the DataFrame.
+    """
+
+    df = handle_missing(df)
+    df = remove_duplicates(df)
+    df = clip_outliers(df)
+
+    return df
+
+
+def handle_missing(df):
+    """
+    Fill missing values:
+    - Numeric: mean if not skewed, else median
+    - Categorical: mode
+    """
+
+    df2 = df.copy()
+    num_cols = df2.select_dtypes(include=np.number).columns
+    cat_cols = df2.select_dtypes(include='object').columns
+
+    # Numeric Columns
+    for col in num_cols:
+        if df2[col].isnull().any():
+            skewness = df2[col].skew()
+            if abs(skewness) > 1:
+                df2[col] = df2[col].fillna(df2[col].median())
+            else:
+                df2[col] = df2[col].fillna(df2[col].mean())
+
+    # Categorical Columns
+    for col in cat_cols:
+        if df2[col].isnull().any():
+            df2[col] = df2[col].fillna(df2[col].mode()[0])
+
+    return df2
+
+
+def remove_duplicates(df):
+    """
+    Remove duplicate rows from the DataFrame.
+    """
+    if df.duplicated().sum() == 0:
+        print("No duplicate rows found.")
+        return df
+
+    len_before = len(df)
+    df = df.drop_duplicates()
+    len_after = len(df)
+    print(f"Removed {len_before - len_after} duplicate rows.")
+    return df
+
+
+def clip_outliers(df):
+    """
+    Clip outliers in numeric columns using the IQR method.
+    """
+
+    df2 = df.copy()
+    num_cols = df2.select_dtypes(include=np.number).columns
+    for col in num_cols:
+        Q1 = df2[col].quantile(0.25)
+        Q3 = df2[col].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+
+        df2[col] = df2[col].clip(lower_bound, upper_bound)
+
+    return df2
